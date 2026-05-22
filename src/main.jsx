417a80563cb33d305599d647d38ee19fd3c813a7
izabelem167-boop <izabelem167@gmail.com>
2026-05-22 14:34:53 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+function App() {
+  return (
+    <div>
+      <h1>MusicAI</h1>
+      <p>Site funcionando 🎵</p>
+    </div>
+  )
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default App
